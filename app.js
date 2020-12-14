84ecc25aedfef4d6f737a2e1db32d0db883111f1
yangel20 <61720403+yangel20@users.noreply.github.com>
2020-12-14 14:41:43 -0500

    just confirming my last push

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+const http = require("http");
+const socketio = require("socket.io");
+
 const express = require("express");
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Data Base stuff
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
 const users = require("./routes/api/users");
@@ -7,13 +14,27 @@ const User = require("./models/User");
 const bodyParser = require("body-parser");
 
 
+const port = process.env.PORT || 5000;
+app.listen(port, () => {console.log(`Listening on port ${port}`)});
+
+io.on("connection", socket => {
+  console.log("New WS Connection ...")
+})
+
+
+
+
+
+// Data Base stuff
 mongoose
-  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connected to mongoDB"))
-  .catch(err => console.log(err))
+.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("Connected to mongoDB"))
+.catch(err => console.log(err))
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use("/api/users", users)
 
 app.get("/", (req, res) => {
   const user = new User({
@@ -22,11 +43,5 @@ app.get("/", (req, res) => {
   })
   // debugger;
   user.save()
-  res.send("Hello World!");
+  res.send("Capicua Server!");
 });
-
-app.use("/api/users", users)
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {console.log(`Listening on port ${port}`)});
