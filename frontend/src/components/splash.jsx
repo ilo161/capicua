4b47cc59eb5fbb6b07d792d6da3d3538b9468c6e
yangel20 <61720403+yangel20@users.noreply.github.com>
2020-12-17 18:23:44 -0500

    start design splash page

---
+++ frontend/src/components/splash.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class Splash extends React.Component {
+
+
+    render(){
+        <div>
+            <nav className="nav-bar">
+            </nav>
+            <div className="pick-game">
+            </div>
+            <div className="MERN-team">
+                    <div className="sergio" >
+
+                    </div>
+                    <div className="steven">
+
+                    </div>
+                    <div className="chris">
+
+                    </div>
+                    <div className="yangel">
+                        
+                    </div>
+            </div>
+        </div>    
+    }
+}
