23599b0b547a2bf8aa4faeb472e3204dfd463934
Sergio Medina <medinaommedina@gmail.com>
2020-12-17 14:51:57 -0800

    remove app.css for merge and import line from app.js for merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,5 @@
 
-import './App.css';
-// import Drawing from "./drawing.jsx"
-// import Bone from "./components/bone.jsx"
-// import Hueso from "./hueso.jsx"
-// import Board from "./components/board"
+
 import Game from "./components/game"
 
 
